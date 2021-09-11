78ce2c8e89415d26d4899bf42c9fc60db19a4469
pedfav <pedfav@gmail.com>
2021-09-10 22:56:20 -0300

    kickoff

---
+++ src/main/java/com/pedfav/pocsqs/PocSqsApplication.java
@@ -0,0 +1,13 @@
+package com.pedfav.pocsqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSqsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocSqsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedfav/pocsqs/PocSqsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedfav.pocsqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocSqsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
